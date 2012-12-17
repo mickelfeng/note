e73e5cea16d5e5031d5b06e1a78224bd3d9cd934
bybzmt <bybzmt@bybzmt.me>
2012-12-17 16:34:14 +0800

    a

--- code.php
+++ code.php
@@ -2,6 +2,7 @@
 
 /**
  * 计算图片旋转时需要的坐标
+ * (旋转圆上的一个点，计算旋转后的坐标)
  */
 
 //期望的坐标
@@ -25,3 +26,23 @@ $p2_y = cos($S) * $D;
 $r_h = mt_rand(16,55) + 0xA0;
 $r_l = mt_rand(1, ($r_h==55)?89:94) + 0xA0;
 $str = mb_convert_encoding(chr($r_h).chr($r_l), 'utf-8', 'gb2312');
+
+/**
+ * 打印gb2312常用字表
+ */
+echo "<table>";
+for ($r_h=16; $r_h<=55; $r_h++) {
+	echo "<tr>";
+	for($r_l=1; $r_l<=($r_h==55?89:94); $r_l++) {
+		$str = chr($r_h+0xA0).chr($r_l+0xA0);
+		echo mb_convert_encoding($str, 'utf-8', 'gb2312');
+	}
+	echo "</tr>";
+}
+echo "</table>";
+
+/**
+ * 随机得到一个中文字符
+ */
+$chr = mt_rand(0x4e00, 0x9fa5);
+echo html_entity_decode("&#{$chr};", ENT_COMPAT, 'utf-8');
